cb333f3bee8b73fa5af8d592456c0819cbb81040
simelpaul4@gmail.com <simelpaul4@gmail.com>
2022-01-20 12:40:55 +0300

    modify

---
+++ Js/script.js
@@ -0,0 +1,16 @@
+ $("#mit1").click(function() {
+        $("#mit").slideToggle();
+    });
+
+$("#mot1").click(function(){
+    $("#mot").slideToggle();
+
+})
+
+$("#met1").click(function(){
+    $("#met").slideToggle();
+})
+
+$("#mat1").click(function() {
+    $("#mat").slideToggle();
+})
